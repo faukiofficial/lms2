fd5434241619fecd48c90ea03b0edd6b1743d434
faukiofficial <faukiofficial@gmail.com>
2025-01-25 16:16:45 +0700

    finsh details

--- client/src/components/student/Loading.tsx
+++ client/src/components/student/Loading.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
-type Props = {}
+type Props = {};
 
-const Loading = (props: Props) => {
+const Loading: React.FC<Props> = () => {
   return (
-    <div>Loading</div>
-  )
-}
+    <div className="flex items-center justify-center h-screen">
+      <div className="w-12 h-12 border-4 border-t-4 border-gray-300 rounded-full animate-spin border-t-blue-500"></div>
+    </div>
+  );
+};
 
-export default Loading
+export default Loading;
--- client/src/pages/student/MyEnrollments.tsx
+++ client/src/pages/student/MyEnrollments.tsx
@@ -4,7 +4,10 @@ type Props = {}
 
 const MyEnrollments = (props: Props) => {
   return (
-    <div>MyEnrollments</div>
+    <div className="relative flex md:flex-row flex-col-reverse gap-10 items-start justify-between px-4 sm:px-10 md:px-14 lg:px-30 md:pt-20 pt-15 text-left">
+          <div className="absolute top-0 left-0 w-full h-[500px] bg-gradient-to-b from-cyan-100/70"></div>
+      
+    </div>
   )
 }
 
